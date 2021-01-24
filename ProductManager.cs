a445f8f7185971f255d882d4f182d904ec92e78a
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-01-24 12:00:03 +0300

    Add project files.

---
+++ ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class ProductManager
+    {   
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName +"Sepetinize Eklendi!");
+        }
+
+        public void Update(Product product) //void; void metodu,
+        {
+            Console.WriteLine(product.ProductName + "Güncellendi.");
+        }
+       
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 20;
+
+            Product product2 = new Product { Id = 2, CategoryId = 3, ProductName = "Sandalye", UnitPrice = 120, UnitsInStock = 80 };
+            // In real life productname  data etc. came from user input.
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
